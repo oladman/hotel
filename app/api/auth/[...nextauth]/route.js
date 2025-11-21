74c078be4cee0007c8fdfe3412c88ae593225a72
Oladimeji Seunayo Ezekiel <oladimejiseunayo@gmail.com>
2025-11-21 23:38:25 +0100

    smch

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,1 +1,7 @@
-export { GET, POST } from "../../../../auth"
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google"; // example
+// import other providers or options
+import { authOptions } from "../../../../auth"; // your auth config
+
+export const GET = (req) => NextAuth(req, authOptions);
+export const POST = (req) => NextAuth(req, authOptions);
